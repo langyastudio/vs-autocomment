e89d8014c48cce90b1b09207836b1a6fc7285037
shilang ao <jiangjiaxiong@hacfin.com>
2019-12-05 10:24:37 +0800

    新增 vs - 2019 版 自动注释

---
+++ AutoComment2019/AutoComment2015/InputBox.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HKFY.AutoComment2015
+{
+    public partial class InputBox : Form
+    {
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+
+        //控制输入数字
+        private void _txtBoxRow_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && (Keys)e.KeyChar != Keys.Back)
+            {
+                e.Handled = true;
+                return;
+            }
+        }
+
+        //控制输入数字
+        private void _txtBoxCol_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && (Keys)e.KeyChar != Keys.Back)
+            {
+                e.Handled = true;
+                return;
+            }
+        }
+
+        //行数
+        public int RowNum
+        {
+            get 
+            {
+                int rowNum = 0;
+                if (int.TryParse(_txtBoxRow.Text, out rowNum) && rowNum > 0)
+                    return (rowNum);
+                return (3);
+            }
+        }
+
+        //列数
+        public int ColNum
+        {
+            get
+            {
+                int colNum = 0;
+                if (int.TryParse(_txtBoxCol.Text, out colNum) && colNum > 0)
+                    return (colNum);
+                return (3);
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+    }
+}
---
+++ AutoComment2019/AutoComment2015/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoComment2015")]
+[assembly: AssemblyDescription("为代码添加注释,支持c++/C#.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("郎涯工作室")]
+[assembly: AssemblyProduct("AutoComment2015")]
+[assembly: AssemblyCopyright("傲十郎")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("zh-Hans")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+
